74a0818346b8e007de3691edeaf34a64cf75cb16
Paul <2zeeno@naver.com>
2021-05-12 18:31:58 +0900

    내비게이션 기능 추가

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import Question
-from django.views import generic
+# from django.views import generic
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 
