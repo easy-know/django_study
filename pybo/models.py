50e7258e76aca2e50b27a8a04048741f59109073
Paul <2zeeno@naver.com>
2021-05-13 17:45:33 +0900

    댓글 기능 추가

--- pybo/models.py
+++ pybo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,6 +8,8 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -16,3 +19,16 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    modify_date = models.DateTimeField(null=True, blank=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    question = models.ForeignKey(
+        Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(
+        Answer, null=True, blank=True, on_delete=models.CASCADE)
