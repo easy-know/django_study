f5d0045038ae1cfc5c6e7cda911f8f8c0ce1048f
Paul <2zeeno@naver.com>
2021-05-14 13:52:14 +0900

    추천 기능 추가

--- pybo/models.py
+++ pybo/models.py
@@ -5,22 +5,26 @@ from django.contrib.auth.models import User
 
 
 class Question(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
     def __str__(self):
         return self.subject
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name='author_answer')
     content = models.TextField()
     create_date = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
 
 class Comment(models.Model):
