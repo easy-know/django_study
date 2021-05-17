497fc1759f9cdca4a54f86f8c7acfcb5e414f77a
Paul <2zeeno@naver.com>
2021-05-17 15:54:27 +0900

    검색, 정렬 추가

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,4 +1,6 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -6,3 +8,12 @@ register = template.Library()
 @register.filter
 def sub(value, arg):
     return value - arg
+
+
+@register.filter()
+def mark(value):
+    '''
+    "nl2br"은 줄바꿈 문자를 <br> 태그로 바꿔 주므로 <Enter>를 한 번만 눌러도 줄바꿈으로 인식한다. 만약 이 확장 도구를 사용하지 않으면 줄바꿈을 위해 줄 끝에 마크다운 문법인 스페이스를 2개를 연속으로 입력해야 할 것이다. "fenced_code"는 마크다운의 소스 코드 표현을 위해 적용했다
+    '''
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
